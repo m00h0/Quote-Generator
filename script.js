c9739925212de42d8b08adcf8d7a4c6c69c8a2b4
newtonah0 <ezmh015@gmail.com>
2025-01-18 22:37:21 +0300

    fix linters errors

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
 async function getQuote() {
-    const proxyUrl = 'https://api.allorigins.win/get?url=';
-    const apiUrl = 'https://zenquotes.io/api/random';
-    const url = proxyUrl + encodeURIComponent(apiUrl) + '&' + new Date().getTime();
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        const contents = JSON.parse(data.contents);
-        const quote = contents[0].q;
-        const author = contents[0].a;
-        const theQuote = document.getElementById('quote');
-        if (quote === '') {
-            theQuote.innerText = 'unknown';
-        }
-        else {
-        theQuote.innerText = quote;
-        }
-        const theAuthor = document.getElementById('author');
-        if (author === '') {
-            theAuthor.innerText = 'unknown';
-        }
-        else {
-        theAuthor.innerText = '- ' + author;
-        }
-        } 
-    catch (error) {
-        getQuote();
+  const proxyUrl = 'https://api.allorigins.win/get?url=';
+  const apiUrl = 'https://zenquotes.io/api/random';
+  const url = `${proxyUrl + encodeURIComponent(apiUrl)}&${new Date().getTime()}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const contents = JSON.parse(data.contents);
+    const quote = contents[0].q;
+    const author = contents[0].a;
+    const theQuote = document.getElementById('quote');
+    if (quote === '') {
+      theQuote.innerText = 'unknown';
+    } else {
+      theQuote.innerText = quote;
     }
+    const theAuthor = document.getElementById('author');
+    if (author === '') {
+      theAuthor.innerText = 'unknown';
+    } else {
+      theAuthor.innerText = `- ${author}`;
+    }
+  } catch (error) {
+    getQuote();
+  }
 }
 
 function quoteX() {
-    const quote = document.getElementById('quote').innerText;
-    const author = document.getElementById('author').innerText;
-    const xUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(quote + ' ' + author)}`;
-    window.open(xUrl, '_blank');
+  const quote = document.getElementById('quote').innerText;
+  const author = document.getElementById('author').innerText;
+  const xUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(`${quote} ${author}`)}`;
+  window.open(xUrl, '_blank');
 }
 
 const newQuote = document.getElementById('quote-button');
 const xQuote = document.getElementById('x');
 newQuote.addEventListener('click', getQuote);
 xQuote.addEventListener('click', quoteX);
-
 
 getQuote();
